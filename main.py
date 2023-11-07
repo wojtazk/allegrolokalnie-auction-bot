e322edeeb5dc0ac030a79a7fa57f42d3204fa773
Wojciech Kowal <w.kowal202@gmail.com>
2023-11-07 11:53:50 +0100

    resolve #5

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import time
-from sys import exit
+import sys
 
 from auction_bot import AuctionBot
 # from user_credentials_dialog import UserCredentialsDialog
@@ -15,13 +15,7 @@ from selenium.common.exceptions import (NoSuchElementException, NoSuchWindowExce
 #     exit(1)
 
 try:
-    try:
-        # ask user to provide information for the bot
-        bot_arguments = ask_user_for_info()
-    except ValueError as err:
-        # handle wrong info
-        print_message(f'{type(err).__name__}: {err.args[0]}', False)
-        exit(1)
+    bot_arguments = ask_user_for_info()
 
     # inform user about logging in
     print_login_info()
@@ -53,9 +47,14 @@ try:
 
         auction_bot.make_an_offer()
 
-except (KeyboardInterrupt, NoSuchElementException, NoSuchWindowException,
+except (KeyboardInterrupt, SystemExit, ValueError, NoSuchElementException, NoSuchWindowException,
         WebDriverException, InvalidSessionIdException) as err:
     print()
     print_message('Bot terminated!', False)
-    print_message(type(err).__name__, False)
-    exit(1)
+
+    if len(err.args) > 0:
+        print_message(f'{type(err).__name__}: {err.args[0]}', False)
+    else:
+        print_message(type(err).__name__, False)
+
+    sys.exit(1)
